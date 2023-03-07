cf0e9d2e835893d992fdeeb7ec9f2b6a9a0fd484
valderez <valderezpe@gmail.com>
2023-03-07 09:37:57 -0300

    API Rest, Flask,  Python,  Mysql

---
+++ bd.py
@@ -0,0 +1,21 @@
+Carros= [
+ {
+        'id':1,
+        'marca': 'Ford',
+        'modelo': 'Ranger',
+        'ano': '2020',
+    },
+    {
+        'id':2,
+        'marca': 'chevrolet',
+        'modelo': 'Gol',
+        'ano': '2019', 
+    },
+      {
+        'id':3,
+        'marca': 'Toyota',
+        'modelo': 'Hilux',
+        'ano': '2023',  
+    }
+ 
+]
